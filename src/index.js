277d819fa19bdf883e5a9b1a7c75955a979b01e6
Jason <jason.mobley5@gmail.com>
2023-01-15 21:57:47 -0500

    import function name change

--- src/index.js
+++ src/index.js
@@ -2,79 +2,14 @@
 
 import { executeSearch, getID } from "./fetchData"
 import { searchBtn } from "./events"
+import { setDataFarenheit } from "./applyData"
 
 
 
-const getLocation = document.getElementById('location')
-const date = document.getElementById('date')
-const tempurature = document.getElementById('tempurature')
-const sunriseText = document.getElementById('sunriseText')
-const sunsetText = document.getElementById('sunsetText')
-const humidity = document.getElementById('humidity')
-const dewPoint = document.getElementById('dewPoint')
-const wind = document.getElementById('wind')
-const visibility = document.getElementById('visibility')
 
 
-const hrTextOne = document.getElementById('hrTextOne')
-const hrTextTwo = document.getElementById('hrTextTwo')
-const hrTextThree = document.getElementById('hrTextThree')
-const hrTextFour = document.getElementById('hrTextFour')
-const hrTextFive = document.getElementById('hrTextFive')
-const hrTextSix = document.getElementById('hrTextSix')
-const hrTextSeven = document.getElementById('hrTextSeven')
-const hrTextEight = document.getElementById('hrTextEight')
 
-const hrImgOne = document.getElementById('hrImgOne')
-const hrImgTwo = document.getElementById('hrImgTwo')
-const hrImgThree = document.getElementById('hrImgThree')
-const hrImgFour = document.getElementById('hrImgFour')
-const hrImgFive = document.getElementById('hrImgFive')
-const hrImgSix = document.getElementById('hrImgSix')
-const hrImgSeven = document.getElementById('hrImgSeven')
-const hrImgEight = document.getElementById('hrImgEight')
 
-const hrTempOne = document.getElementById('hrTempOne')
-const hrTempTwo = document.getElementById('hrTempTwo')
-const hrTempThree = document.getElementById('hrTempThree')
-const hrTempFour = document.getElementById('hrTempFour')
-const hrTempFive = document.getElementById('hrTempFive')
-const hrTempSix = document.getElementById('hrTempSix')
-const hrTempSeven = document.getElementById('hrTempSeven')
-const hrTempEight = document.getElementById('hrTempEight')
 
-const forecastText = document.getElementById('forecastText')
-
-const dayDateOne = document.getElementById('dayDateOne')
-const dayDateTwo = document.getElementById('dayDateTwo')
-const dayDateThree = document.getElementById('dayDateThree')
-const dayDateFour = document.getElementById('dayDateFour')
-const dayDateFive = document.getElementById('dayDateFive')
-const dayDateSix = document.getElementById('dayDateSix')
-const dayDateSeven = document.getElementById('dayDateSeven')
-
-const dayImgOne = document.getElementById('dayImgOne')
-const dayImgTwo = document.getElementById('dayImgTwo')
-const dayImgThree = document.getElementById('dayImgThree')
-const dayImgFour = document.getElementById('dayImgFour')
-const dayImgFive = document.getElementById('dayImgFive')
-const dayImgSix = document.getElementById('dayImgSix')
-const dayImgSeven = document.getElementById('dayImgSeven')
-
-const forecastOne = document.getElementById('forecastOne')
-const forecastTwo = document.getElementById('forecastTwo')
-const forecastThree = document.getElementById('forecastThree')
-const forecastFour = document.getElementById('forecastFour')
-const forecastFive = document.getElementById('forecastFive')
-const forecastSix = document.getElementById('forecastSix')
-const forecastSeven = document.getElementById('forecastSeven')
-
-const dayRainOne = document.getElementById('dayRainOne')
-const dayRainTwo = document.getElementById('dayRainTwo')
-const dayRainThree = document.getElementById('dayRainThree')
-const dayRainFour = document.getElementById('dayRainFour')
-const dayRainFive = document.getElementById('dayRainFive')
-const dayRainSix = document.getElementById('dayRainSix')
-const dayRainSeven = document.getElementById('dayRainSeven')
 
 executeSearch()
