76995bd84d468bfad071f569fb88c14b6cc117ac
Jason <jason.mobley5@gmail.com>
2023-01-18 06:25:47 -0500

    Now fetches data with metric/celcius values

--- src/fetchData.js
+++ src/fetchData.js
@@ -2,17 +2,13 @@
 
 // import{}
 
-export {executeSearch, getOptions, selectedData}
+export {executeSearch, getOptions, selectedDataUS, selectedDataMetric}
 
 const searchBar = document.getElementById('searchBar')
 const searchModal = document.getElementById('searchModal')
 
-let optionOne
-let optionTwo
-let optionThree
-let optionFour
-let optionFive
-let selectedData
+let selectedDataUS
+let selectedDataMetric
 
 // ASYNC FUNCTIONS
 
@@ -28,20 +24,12 @@ async function getOptions() {
     try {
         let response = await fetch (`http://api.openweathermap.org/geo/1.0/direct?q=${searchBar.value}&limit=5&appid=b553abef78d7459ae8fd13980b3e30b0`, {mode: 'cors'})
         let fetchedData = await response.json()
-        console.log(fetchedData)
-
-        optionOne = [`${fetchedData[0].name}, ${fetchedData[0].state}, ${fetchedData[0].country}`, fetchedData[0].lat, fetchedData[0].lon ];
-        optionTwo = [`${fetchedData[1].name}, ${fetchedData[1].state}, ${fetchedData[1].country}`, fetchedData[1].lat, fetchedData[1].lon];
-        optionThree = [`${fetchedData[2].name}, ${fetchedData[2].state}, ${fetchedData[2].country}`, fetchedData[2].lat, fetchedData[2].lon ];
-        optionFour = [`${fetchedData[3].name}, ${fetchedData[3].state}, ${fetchedData[3].country}`, fetchedData[3].lat, fetchedData[3].lon ];
-        optionFive = [`${fetchedData[4].name}, ${fetchedData[4].state}, ${fetchedData[4].country}`, fetchedData[4].lat, fetchedData[4].lon ];
+        console.log(fetchedData) // DELETE
 
         createModalHeader()
-        createOption(optionOne[0], optionOne[1], optionOne[2])
-        createOption(optionTwo[0], optionTwo[1], optionTwo[2])
-        createOption(optionThree[0], optionThree[1], optionThree[2])
-        createOption(optionFour[0], optionFour[1], optionFour[2])
-        createOption(optionFive[0], optionFive[1], optionFive[2])
+        fetchedData.forEach(option => {
+            createOption(`${option.name}, ${option.state}, ${option.country}`)
+        })
         createModalClose()
         searchModal.showModal()
     } catch {
@@ -51,22 +39,19 @@ async function getOptions() {
     }
 }
 
-async function fetchToday(location) {
-    
-}
-
-async function fetchHourly(lat, lon) {
-    let response = await fetch(``)
-}
-
-async function fetchDaily(location) {
-    let response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/?key=J8KPLV9423DYLDLRH3MVXGR3S`, {mode: 'cors'})
+async function fetchDailyUS(location) {
+    let response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/?unitGroup=us&key=J8KPLV9423DYLDLRH3MVXGR3S`, {mode: 'cors'})
     let fetchedData = await response.json()
-    selectedData = fetchedData
-    console.log(selectedData)
+    selectedDataUS = fetchedData
+    console.log(selectedDataUS)
 }
 
-
+async function fetchDailyMetric(location) {
+    let response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/?unitGroup=metric&key=J8KPLV9423DYLDLRH3MVXGR3S`, {mode: 'cors'})
+    let fetchedData = await response.json()
+    selectedDataMetric = fetchedData
+    console.log(selectedDataMetric)
+}
 
 // FUNCTIONS
 
@@ -75,7 +60,10 @@ function createOption(option) {
     optionBtn.classList.add('option-button')
     optionBtn.textContent = option
     optionBtn.onclick = () => {
-        fetchDaily(option)
+        fetchDailyUS(option)
+        fetchDailyMetric(option)
+        searchModal.replaceChildren()
+        searchModal.close()
     }
     searchModal.appendChild(optionBtn)
 }
@@ -103,20 +91,3 @@ function createModalClose() {
     closeBtn.textContent = 'Close & Try Again'
     searchModal.appendChild(closeBtn)
 }
-
-function resetOptions() {
-    optionOne = ''
-    optionTwo = ''
-    optionThree = ''
-    optionFour = ''
-    optionFive = ''
-}
-
-
-
-
-
-
-
-
-
