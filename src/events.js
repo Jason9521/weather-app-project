a898c8fee42d111f7fc8daf750aa6545b01ab6e6
Jason <jason.mobley5@gmail.com>
2023-01-21 23:21:32 -0500

    Added listener for toggle button

--- src/events.js
+++ src/events.js
@@ -1,11 +1,20 @@
 "use strict"
 
 import { getOptions } from "./fetchData"
-export {searchBtn}
+export {searchBtn, tempToggle}
 
 const searchBtn = document.getElementById('searchBtn')
+const tempToggle = document.getElementById('tempToggle')
 
 searchBtn.addEventListener('click', () => {
     getOptions()
+})
+
+tempToggle.addEventListener('click', () => {
+    if (tempToggle.checked) {
+        // Display Celcius Data
+    } else {
+        // Display Farhenheit Data
+    }
 })
 
