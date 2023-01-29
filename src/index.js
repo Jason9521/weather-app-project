f8ee0569fee4fc0b34140957a760a7097fdefa47
Jason <jason.mobley5@gmail.com>
2023-01-29 17:27:32 -0500

    changed import link

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 "use strict"
 
-import './src/style.css'
+import './style.css'
 import { getID } from "./fetchData"
 import { searchBtn, tempToggle } from "./events"
 import { setDataFahrenheit, displayDefault } from "./applyData"
