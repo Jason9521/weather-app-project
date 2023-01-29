57b9fb3456b6a77c0e12c0b0947ca0e80cb6610f
Jason <jason.mobley5@gmail.com>
2023-01-29 17:01:29 -0500

    imported stylesheet

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 "use strict"
 
+import './style.css'
 import { getID } from "./fetchData"
 import { searchBtn, tempToggle } from "./events"
 import { setDataFahrenheit, displayDefault } from "./applyData"
